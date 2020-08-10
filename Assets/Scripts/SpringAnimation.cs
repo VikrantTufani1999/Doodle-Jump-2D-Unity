bf73bb35c70020f48e432a5a2b58825ab17dd849
Vikrant Tufani <8052007294vicky@gmail.com>
2020-08-10 13:17:14 +0530

    Updated 10.08.2020

---
+++ Assets/Scripts/SpringAnimation.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpringAnimation : MonoBehaviour
+{
+    Animator springAnim;
+
+
+    void Start()
+    {
+        springAnim = GetComponent<Animator>();    
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            springAnim.SetBool("spring", true);
+        }
+    }
+
+    
+}
